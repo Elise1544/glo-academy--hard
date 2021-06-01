52b0c11feb2497d8db5b5d3c78c3b093f988e91f
Elizaveta Zabrodskaya <e.bagrintseva@gmail.com>
2021-06-01 18:23:52 +0300

    Добавляет функцию для вывода строки

--- js/index.js
+++ js/index.js
@@ -1,39 +1,15 @@
 'use strict';
 
-let lang = 'ru';
-
-let daysOnRussian = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'];
-let daysOnEnglish = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
-
-if (lang == 'ru') {
-  console.log(daysOnRussian);
-} else if (lang == 'en') {
-  console.log(daysOnEnglish);
+function showString(str) {
+  if (typeof(str) !== 'string') {
+    return ('Введите строку');
+  } else {
+    str = str.trim();
+    if (str.length > 30) {
+      str = str.slice(0, 30) + '...';
+    }
+    return str;
+  }
 }
 
-switch(lang) {
-  case 'ru':
-    console.log(daysOnRussian);
-    break;
-  case 'en':
-    console.log(daysOnEnglish);
-    break;
-}
-
-let days = {
-  'ru': daysOnRussian,
-  'en': daysOnEnglish,
-};
-
-console.log(days[lang]);
-
-let lang_array = [];
-lang_array['ru'] = daysOnRussian;
-lang_array['en'] = daysOnEnglish;
-console.log(lang_array[lang]);
-
-let namePerson = 'Артем';
-
-namePerson === 'Артем' ? console.log('директор') : 
-namePerson === 'Максим' ? console.log('преподаватель') : 
-console.log('студент');
+console.log(showString('            11231321123123012321321321321312                        '));
