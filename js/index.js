f7644d7dbd3bec3712240e4ff32f82d394eec59b
Elizaveta Zabrodskaya <e.bagrintseva@gmail.com>
2021-06-02 20:53:38 +0300

    Добавляем циклы

--- js/index.js
+++ js/index.js
@@ -1,15 +1,30 @@
 'use strict';
 
-function showString(str) {
-  if (typeof(str) !== 'string') {
-    return ('Введите строку');
-  } else {
-    str = str.trim();
-    if (str.length > 30) {
-      str = str.slice(0, 30) + '...';
+let arr = ['22', '36', '494', '838', '3891', '250', '6020'];
+
+function showNumbers(array) {
+  for (let i = 0; i < array.length; i++) {
+    if (array[i][0] === '2' || array[i][0] === '4') {
+      console.log(array[i]);
     }
-    return str;
   }
 }
 
-console.log(showString('            11231321123123012321321321321312                        '));
+showNumbers(arr);
+
+function showPrimeNumber() {
+  for (let i = 1; i <= 100; i++) {
+    let flag = false;
+    for (let k = 2; k < i; k++) {
+      if (i % k === 0) {
+        flag = true;
+        break;
+      }
+    }
+    if (flag === false) {
+      console.log(`${i}, Делители этого числа: 1 и ${i}`);
+    }
+  }
+}
+
+showPrimeNumber();
