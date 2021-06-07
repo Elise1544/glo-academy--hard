f2e722255087fc8232a0d23de474d0dff881946b
Elizaveta Zabrodskaya <e.bagrintseva@gmail.com>
2021-06-07 18:27:33 +0300

    Выводит дату и время каждую секунду

--- js/index.js
+++ js/index.js
@@ -1,14 +1,64 @@
 'use strict';
 
-let week = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'];
-let data = new Date();
 
-for (let i = 0; i < week.length; i++) {
-  if (((6 + data.getDay()) % 7) === i) {
-    document.write(`<p>${week[i].bold()}</p>`);
-  } else if (i === 6 || i === 5) {
-    document.write(`<p>${week[i].italics()}</p>`);
-  } else {
-    document.write(`<p>${week[i]}</p>`);
+function showDate() {
+  let date;
+
+  const monthNames = ['Января', 'Февраля', 'Марта', 'Апреля', 'Мая', 'Июня',
+    'Июля', 'Августа', 'Сентября', 'Октября', 'Ноября', 'Декабря'];
+  const dayNames = ['Воскресение', 'Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота'];
+
+  function showHours() {
+    switch (date.getHours()) {
+      case 1:
+      case 21:
+        return `${date.getHours()} час`;
+      case 2:
+      case 3:
+      case 4:
+      case 22:
+      case 23:
+        return `${date.getHours()} часа`;
+      default:
+        return `${date.getHours()} часов`;
+    }
+  };
+
+  function showTime(time, interval) {
+    if (time == 1 || time == 21 || time == 31 || time == 41 || time == 51) {
+      return `${interval}а`;
+    } else if ((time >= 2 && time <= 4) ||
+      (time >= 22 && time <= 24) ||
+      (time >= 32 && time <= 34) ||
+      (time >= 42 && time <= 44) ||
+      (time >= 52 && time <= 54)) {
+      return `${interval}ы`;
+    } else {
+      return `${interval}`;
+    }
+  };
+
+  function showFullDate() {
+    date = new Date();
+    return `Сегодня ${dayNames[date.getDay()]}, ${date.getDate()} ${monthNames[date.getMonth()]} ${date.getFullYear()} года, ${showHours()} ${date.getMinutes()} ${showTime(date.getMinutes(), 'минут')} ${date.getSeconds()} ${showTime(date.getSeconds(), 'секунд')}`
   }
+
+  function addZero(dd) {
+    if (dd < 10) {
+      dd = '0' + dd;
+    }
+    return dd;
+  }
+  
+  function showShortDate () {
+    date = new Date();
+    return `${addZero(date.getDate())}.${addZero(date.getMonth())}.${addZero(date.getFullYear())} - ${addZero(date.getHours())}:${addZero(date.getMinutes())}:${addZero(date.getSeconds())}`
+  }
+
+  setInterval(function () {
+    console.log(`a) '${showFullDate()}'`);
+    console.log(`б) '${showShortDate()}'`)
+  }, 1000);
 }
+
+showDate();
