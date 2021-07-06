4bde23897e7d36e6b143c7f4e546a335f20b3412
Elizaveta Zabrodskaya <e.bagrintseva@gmail.com>
2021-07-06 17:43:36 +0300

    Добавляем пользователей

--- js/index.js
+++ js/index.js
@@ -1,16 +1,53 @@
 'use strict';
 
-const button = document.querySelector('button');
-const p = document.querySelector('p');
+const username = document.getElementById('username'),
+  registerUser = document.getElementById('registerUser'),
+  login = document.getElementById('login'),
+  list = document.getElementById('list');
 
-p.style.textAlign = 'center';
-button.style.margin = '0 auto';
-button.style.display = 'block';
-
-function newColor () {
-  let randomColor = Math.floor(Math.random()*16777215).toString(16);
-  document.body.style.backgroundColor = '#' + randomColor;
-  p.innerHTML = '#' + randomColor;
+let users;
+if (localStorage.getItem('users') !== null) {
+  users = JSON.parse(localStorage.getItem('users'));
+} else {
+  users = [];
 }
 
-button.addEventListener('click', newColor);
+const addUser = () => {
+  let fullName = prompt('Введите имя и фамилию');
+  let fullNameArray = fullName.split(' ');
+  if (fullNameArray.length > 2) {
+    alert('Некорректное значение');
+    addUser();
+  } else {
+    let date = new Date().toLocaleString('ru', {
+      year: 'numeric',
+      month: 'long',
+      day: 'numeric',
+      hour: 'numeric',
+      minute: 'numeric',
+      second: 'numeric'
+    });
+    let newUser = {
+      firstName: fullNameArray[0],
+      lastName: fullNameArray[1],
+      regDate: date,
+      login: prompt('Введите логин'),
+      password: prompt('Введите пароль')
+    }
+    users.push(newUser);
+    localStorage.setItem('users', JSON.stringify(users));
+    showOnPage();
+  }
+}
+
+const showOnPage = () => {
+  list.innerHTML = '';
+  for (let i = 0; i < users.length; i++) {
+    let li = document.createElement('li');
+    li.textContent = `Имя: ${users[i].firstName} Фамилия: ${users[i].lastName} Дата регистрации: ${users[i].regDate}`
+    list.append(li);
+  }
+}
+showOnPage();
+
+registerUser.addEventListener('click', addUser)
